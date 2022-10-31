c5c4d05c76162b626a637ecde3e525fe168e5abf
morganSuch <63384862+morganSuch@users.noreply.github.com>
2022-10-31 13:58:56 -0700

    more updates

---
+++ Network/menu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import tkinter as tk
+from tkinter import ttk
+
+def loginMenu():
+    root = tk.Tk()
+    root.title('Login')
+    root.geometry("350x220")
+
+
+    fields = {}
+
+    fields['username_label'] = ttk.Label(text='Username:')
+    fields['username'] = ttk.Entry()
+
+    fields['password_label'] = ttk.Label(text='Password:')
+    fields['password'] = ttk.Entry(show="*")
+
+
+    for field in fields.values():
+        field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X)
+
+    ttk.Button(text='Login').pack(anchor=tk.W, padx=10, pady=5)
+
+    root.mainloop()
