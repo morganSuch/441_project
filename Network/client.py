6bb4f11d69092bae258c84768aa76dfc1d6d4394
morganSuch <msuch@hawk.iit.edu>
2022-11-03 15:03:28 -0500

    new commits

--- Network/client.py
+++ Network/client.py
@@ -4,8 +4,8 @@ import socket
 
 from menu import *
 
-HOST = socket.gethostname()
-# HOST = "127.0.0.1"  # The server's hostname or IP address
+#HOST = socket.gethostname()
+HOST = "169.254.177.83"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 # create socket object
@@ -13,10 +13,17 @@ print("Starting Client")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # connect to server
     print("Connecting to server...")
-    s.connect((HOST, PORT))
-    
+    try:
+        s.connect((HOST, PORT))
+    except:
+        print("connection failed")
     message = input("->")
     while message.lower().strip() != 'bye':
+<<<<<<< Updated upstream
+=======
+#        if(message.lower().strip() == 'menu'):
+#            authenticate()
+>>>>>>> Stashed changes
         s.send(message.encode())
         data = s.recv(1024).decode()
         print("Received from server: " + data)
@@ -26,3 +33,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
 
 print(f"Received {message!r}")
+
